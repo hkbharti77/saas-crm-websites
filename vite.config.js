07fd4d7a14840fabc6599f808a5011923e805a1c
himanshu bharti <hkbharti777@outlook.com>
2026-08-13 10:45:43 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -162,6 +162,7 @@ export default defineConfig(({ mode }) => {
       }),
     ],
     build: {
+      chunkSizeWarningLimit: 1500,
       rollupOptions: {
         output: {
           manualChunks(id) {
